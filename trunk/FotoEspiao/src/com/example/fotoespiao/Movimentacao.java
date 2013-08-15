82f2fd463b322ebaaee672de134d62146bac24a2
carolinerosa.nave <carolinerosa.nave@23f8f0ad-6c69-f293-6958-f10e2ccb6b18>
2013-08-15 19:56:24 +0000

    Movimentacao iniciada.

--- trunk/FotoEspiao/src/com/example/fotoespiao/Movimentacao.java
+++ trunk/FotoEspiao/src/com/example/fotoespiao/Movimentacao.java
@@ -1,21 +1,26 @@
 package com.example.fotoespiao;
 
 import android.graphics.Rect;
+import android.util.Log;
+
 
 public class Movimentacao {
 
 	int x;
 	int y;
+	public Rect Person = new Rect();
+	public Movimentacao(int x, int y) {
 
-	public Movimentacao() {
-
+		
 	}
 
 	public void movimentar(int posX, int posY, Rect rect) {
-
-		this.x = posX;
-		this.y = posY;
-		rect.set(posX, posY, posX + rect.width(), posY + rect.height());
+		Log.i("foi", "down baby down !! ");
+		rect.set(rect.left+posX, rect.top+posY, rect.right+posX, rect.bottom+posY);
+		
+	}
+	public Rect getPosition(){
+		return Person;
 	}
 
 }
