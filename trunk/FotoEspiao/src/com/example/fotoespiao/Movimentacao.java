ba6fc6c1eb1ae0375dc4a10025e15565bc5b0b8c
manoelabarreto.nave <manoelabarreto.nave@23f8f0ad-6c69-f293-6958-f10e2ccb6b18>
2013-08-16 13:42:25 +0000

    Rects Obstaculos feitos, personagem não sai da tela, e primeira fase montada.

--- trunk/FotoEspiao/src/com/example/fotoespiao/Movimentacao.java
+++ trunk/FotoEspiao/src/com/example/fotoespiao/Movimentacao.java
@@ -3,24 +3,27 @@ package com.example.fotoespiao;
 import android.graphics.Rect;
 import android.util.Log;
 
-
 public class Movimentacao {
 
 	int x;
 	int y;
 	public Rect Person = new Rect();
+
 	public Movimentacao(int x, int y) {
 
-		
+		this.x = x;
+		this.y = y;
+
 	}
 
 	public void movimentar(int posX, int posY, Rect rect) {
 		Log.i("foi", "down baby down !! ");
-		rect.set(rect.left+posX, rect.top+posY, rect.right+posX, rect.bottom+posY);
-		
-	}
-	public Rect getPosition(){
-		return Person;
+		if (rect.left + posX >= 0 && rect.top + posY >= 0
+				&& rect.right + posX <= x && rect.bottom + posY <= y){
+			rect.set(rect.left + posX, rect.top + posY, rect.right + posX,
+					rect.bottom + posY);
+		}
+
 	}
 
 }
